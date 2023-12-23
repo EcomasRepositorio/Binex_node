970656840fa1851ba5c128a749c49174c2fc6240
jeferson <jefersonyanac29@gmail.com>
2023-12-23 11:30:39 -0500

    view erros details

--- database/db.js
+++ database/db.js
@@ -5,11 +5,13 @@ const conexion = mysql.createConnection({
     password : 'root',
     database : 'Binex_db'  
 });
-conexion.connect((error)=>{
-    if (error) {
-      console.error('El error de conexión es: ' + error);
+conexion.connect((error) => {
+  if (error) {
+      console.error('Error de conexión:', error.code); // Agrega la impresión del código de error
+      console.error(error); // Muestra el error completo para obtener más detalles
       return;
-    }
-    console.log('¡Conectado a la Base de Datos!');
-  });
+  }
+  console.log('¡Conectado a la Base de Datos!');
+});
+
 module.exports = conexion;
