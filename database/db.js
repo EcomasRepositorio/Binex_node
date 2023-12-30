e1699bbe8f4234cbe220e7b1aa47405dd10c5cc3
jhoosy <maycolmedina366@gmail.com>
2023-12-30 00:31:02 -0500

    realice un pequeño cambio

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const conexion = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'root',
+    password : '',
     database : 'binex_db'  
 });
 conexion.connect((error) => {
