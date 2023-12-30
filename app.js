072672e337954a6d61f81b05c1c87700f331c015
jhoosy <maycolmedina366@gmail.com>
2023-12-29 23:57:27 -0500

    agregue nuevos certi

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.static('public'));
 app.use('/pdfs', express.static(path.join(__dirname, 'PDF_BINEX')));
 
 // Configuración de certificado y clave
-const certificatePath = path.resolve(__dirname, 'certificate.pem');
-const privateKeyPath = path.resolve(__dirname, 'private-key.pem');
+const certificatePath = '/etc/letsencrypt/live/www.binex.edu.pe/fullchain.pem';
+const privateKeyPath = '/etc/letsencrypt/live/www.binex.edu.pe/privkey.pem';
 const credentials = {
   key: fs.readFileSync(privateKeyPath),
   cert: fs.readFileSync(certificatePath),
